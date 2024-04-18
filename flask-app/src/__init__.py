6e2871f4f937c5d1a2bf6e10255fbb1de3673405
ashlynnbraisted <122566610+ashlynnbraisted@users.noreply.github.com>
2024-04-18 16:58:44 -0400

    new init

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -16,10 +16,9 @@ def create_app():
 
     # these are for the DB object to be able to connect to MySQL. 
     app.config['MYSQL_DATABASE_USER'] = 'root'
-    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_root_password.txt').readline().strip()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'project'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
@@ -32,14 +31,16 @@ def create_app():
     def welcome():
         return "<h1>Welcome to the 3200 boilerplate app</h1>"
 
-    # Import the various Beluprint Objects
-    from src.customers.customers import customers
-    from src.products.products  import products
+    # Import the various Blueprint Objects
+    from src.media.media import media
+    from src.profile.profile import profile
+    from src.journal.journal import journal
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(media,   url_prefix='/m')
+    app.register_blueprint(profile,   url_prefix='/p')
+    app.register_blueprint(journal,   url_prefix='/j')
 
     # Don't forget to return the app object
     return app
